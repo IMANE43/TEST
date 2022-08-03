2393aba06db89e98eaa29f008ca511d414180f8d
IMANE43 <imaneaitzidane1@gmail.com>
2022-08-03 19:25:36 +0200

    jjj

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b,res;
+    char opera;
+    printf("You should select a number!: \n");
+    scanf("%d",&a);
+    printf("You should select a number!: \n");
+    scanf(" %d",&b);
+    printf("You should select the operator! \n");
+    scanf(" %c",&opera);
+    switch(opera){
+      case '+': printf("a+b=%d",a+b);
+           break;
+      case '-':printf("a-b=%d",a-b);
+           break;
+      case '*':printf("a*b=%d",a*b);
+           break;
+      case '/': if(b!=0)
+        printf("a/b=%d",a/b);
+           else
+        printf("The operation is impossible with 0\n");
+           break;
+    default : printf("The operation is incorrect\n");
+    break;
+    }
+    return 0;
+
+}
